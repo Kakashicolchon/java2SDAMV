57fd02c7a2b4e81993e8608682cff04b9c2709a5
jitsu <jitsu@DESKTOP-BE3F8KF>
2018-11-20 14:21:09 +0100

    Finished beta

--- Backup_fitxers_en_format_GZIP/src/GZipCompression.java
+++ Backup_fitxers_en_format_GZIP/src/GZipCompression.java
@@ -1,6 +1,8 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,7 +11,7 @@ import java.util.Scanner;
 import java.util.zip.GZIPOutputStream;
 
 public class GZipCompression {
-
+	
 	public GZipCompression() {
 		super();
 
@@ -20,7 +22,7 @@ public class GZipCompression {
 		boolean a = true;
 		Scanner s = new Scanner(System.in);
 		Path pathOrigin = null;
-		
+
 		while (i) {
 			pathOrigin = Paths.get(pathOriginString);
 
@@ -28,7 +30,7 @@ public class GZipCompression {
 				System.out.println("El directori no existeix.");
 				System.out.print("Torna a introduïr el path: ");
 				pathOriginString = s.nextLine();
-				
+
 			}
 			else if (!Files.isDirectory(pathOrigin)) {
 				System.out.println("El path proporcionat no acaba en un directori");
@@ -91,22 +93,23 @@ public class GZipCompression {
 
 	}
 
-	public void 
-	Compression (String pathOriginString , String pathDestinyString) {
+	public void Compression (String pathOriginString , String pathDestinyString) {
+		//Creem txt per poder guardar el nom dels directoris 
+
 		//Abrir carpeta 
 		try {
 			Files.walk(Paths.get(pathOriginString)).forEach(ruta-> {
-			    if (Files.isRegularFile(ruta)) {
-			        
-			    	System.out.println(ruta);
-			    	
-			    	String rutaString = ruta.toString();
-			    	
-			    	
-			    	try {
+				if (Files.isRegularFile(ruta)) {
+
+					System.out.println(ruta);
+
+					String rutaString = ruta.toString();
+
+
+					try {
 						FileInputStream fis = new FileInputStream(rutaString);
 						//path de destino archivo 
-						FileOutputStream fos = new FileOutputStream(pathDestinyString + ruta.getName() );
+						FileOutputStream fos = new FileOutputStream(pathDestinyString + "/" + ruta.getFileName() + ".gz");
 						GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
 						byte[] buffer = new byte[1024];
 						int len;
@@ -118,19 +121,50 @@ public class GZipCompression {
 						fos.close();
 						fis.close();
 					} catch (IOException e) {
-						e.printStackTrace();
+						System.out.println("Error inesperat.");
 					}
-			    	
-			    	
-			    	
-			    }
+
+				}
+				else if (Files.isDirectory(ruta)) {
+					Path pathDestiny = Paths.get(pathDestinyString);
+					Path pathOrigin = Paths.get(pathOriginString);
+					escriureFitxers(pathDestiny, pathOrigin, ruta);
+				}
 			});
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			System.out.println("Error inesperat.");
 		}
-		
-		
+
+
+	}
+	public void escriureFitxers(Path rutaDestiny, Path rutaOrigin, Path ruta) {
+		String rutaDirectoris = rutaDestiny + "/directoris.txt";
+		File archivo = new File(rutaDirectoris);
+		BufferedWriter bw = null;
+
+		if(archivo.exists()) {
+			try {
+				bw = new BufferedWriter(new FileWriter(archivo));
+				bw.write(ruta.getFileName().toString());
+			} catch (IOException e) {
+				System.out.println("Error inesperat.");
+			}
+
+		} else {
+			try {
+				bw = new BufferedWriter(new FileWriter(archivo));
+				bw.write(ruta.getFileName().toString());
+			} catch (IOException e) {
+				System.out.println("Error inesperat.");
+			}
+
+		}
+		try {
+			bw.close();
+		} catch (IOException e) {
+			System.out.println("Error inesperat.");
+		}
+
 	}
 
 
